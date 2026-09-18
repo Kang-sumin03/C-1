487b206815af755d13acd1eef64b0459b9265d79
Kang-sumin03 <mpmsmile@naver.com>
2026-09-18 14:09:00 +0900

    Add files via upload

---
+++ FormatString.cpp
@@ -0,0 +1,18 @@
+/*#include <stdio.h>
+
+int main(void)
+{
+	int myAge = 12;
+	printf("제 나이는 10진수로 %d살, 16진수로 %X살입니다. \n", myAge, myAge);
+	return 0;
+}*/
+
+#include <stdio.h>
+
+int main(void)
+{
+	int num1 = 7, num2 = 13;
+	printf("%o %#o \n", num1, num1);
+	printf("%x %#x \n", num2, num2);
+	return 0;
+}
---
+++ for.cpp
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main(void)
+{
+	int num;
+	for (num = 0; num < 3; num++)
+	{
+		printf("Hi~");
+	}
+}
